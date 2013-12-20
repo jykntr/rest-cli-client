67a7a0450789ad039b9bbc10ea5879ab95469208
Jay Knitter <jayknitter@gmail.com>
2013-12-20 14:42:14 -0700

    Preparing for distribution on PyPi

--- setup.py
+++ setup.py
@@ -2,15 +2,16 @@ from setuptools import setup, find_packages
 setup(
     name="restcli",
     version="0.1",
-    packages=find_packages(),
+    packages=find_packages(exclude='test'),
     entry_points={
         'console_scripts': ['restcli = restcli.restcli:main']
     },
-    install_requires=['requests>=2.0', 'Pygments>=1.6', 'colorama>=0.2.5'],
+    install_requires=['requests>=2.0.0', 'Pygments>=1.6', 'colorama>=0.2.5'],
     package_data={
         'restcli': ['*.conf'],
     },
 
+    author='jykntr',
     author_email="jykntr@gmail.com",
     description="Command line REST request tool",
     license="MIT",
