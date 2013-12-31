f39f86db9d2fb9516ab8575a140d80175e8b9b62
Jay Knitter <jayknitter@gmail.com>
2013-12-31 16:03:03 -0700

    Parameterized version string used in setup.py

--- setup.py
+++ setup.py
@@ -1,7 +1,9 @@
 from setuptools import setup, find_packages
+import restcli
+
 setup(
     name='restcli',
-    version='0.1',
+    version=restcli.__version__,
     packages=find_packages(exclude='test'),
     entry_points={
         'console_scripts': ['restcli = restcli.restcli:main']
@@ -17,5 +19,5 @@ setup(
     license='MIT',
     keywords='rest http',
     url='http://jykntr.github.io/rest-cli-client/',
-    download_url='https://github.com/jykntr/rest-cli-client/releases/tag/v0.1'
+    download_url='https://github.com/jykntr/rest-cli-client/releases/tag/v' + restcli.__version__
 )
